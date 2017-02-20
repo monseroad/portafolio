a4ffd2d5bee70b12ca53337707e99653c15b7d94
monseroad <monsepino@gmail.com>
2017-02-20 01:01:31 -0300

    correccion header

--- js/init.js
+++ js/init.js
@@ -1,9 +1,22 @@
+//EFECTO PARALLAX DE LA PORTADA (IMAGEN DE FONDO)
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
-  }); // end of document ready
+  });
 })(jQuery); // end of jQuery name space
 
+//GALERIA DE IMAGENES (SECCION "PROYECTOS")
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+});
+       
+//IMAGENES (SECCIÓN HERRAMIENTAS)
+$(".foo, .bar").smoove({
+    offset  : '15%',
+    // moveX is overridden to -200px for ".bar" object
+    moveX   : '100px',
+    moveY   : '100px',
+  });
